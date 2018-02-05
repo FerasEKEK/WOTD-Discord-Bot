6a60d58c4ada155339d94c8d11b65fd495ee663f
FirasAKAK <fir_khalidi@hotmail.com>
2018-02-05 09:01:52 +0200

    Update wotdbot.js

--- wotdbot.js
+++ wotdbot.js
@@ -48,7 +48,7 @@ var bot = new Discord.Client({
    autorun: true
 });
 bot.on('ready', function (evt) {
-    bot.user.setUsername("MyNewUsername");
+    bot.setUsername("WodOfDaDayMan");
     logger.info('Connected');
     logger.info('Logged in as: ');
     logger.info(bot.username + ' - (' + bot.id + ')');
